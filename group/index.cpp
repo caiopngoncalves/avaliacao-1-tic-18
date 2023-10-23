0927aa0af7b8486fdcd5dfa0a3d58279fb6a5284
Marcos Vinicius <mvtgomes.ege@uesc.br>
2023-10-23 17:28:54 -0300

    Implementada a classe paciente

--- group/index.cpp
+++ group/index.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Paciente
+{
+
+    string CPF;
+    string DtNascimento;
+
+public:
+    string Nome;
+
+    Paciente(string _Nome, string _CPF, string _DtNascimento)
+    {
+        this->Nome = _Nome;
+        this->setCPF(_CPF);
+        this->setDtNascimento(_DtNascimento);
+    }
+
+    string getCPF()
+    {
+        return this->CPF;
+    }
+
+    void setCPF(string _CPF)
+    {
+        this->CPF = _CPF;
+    }
+
+    string getDtNascimento()
+    {
+        return this->DtNascimento;
+    }
+
+    void setDtNascimento(string _DtNascimento)
+    {
+
+        this->DtNascimento = _DtNascimento;
+    }
+};
 
 int main()
 {
